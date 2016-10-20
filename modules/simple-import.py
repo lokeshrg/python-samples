f269a151e2d5e2bcdde427ce883d168252d4a5be
lokeshrg <gokullokesh@gmail.com>
2016-10-20 15:38:37 +0530

    py cache concept, __name__ and __main__ concepts
    pycache is the compiled python code, cached locally for faster
    performance, __name__ is used to differentiate the internal and external
    usages[runs] of a script[module]

--- modules/simple-import.py
+++ modules/simple-import.py
@@ -6,3 +6,5 @@ print(simplemath.testfn(10))
 # help(math.sin)
 
 # help("modules")
+
+print(simplemath.tf2())
--- modules/simplemath.py
+++ modules/simplemath.py
@@ -1,2 +1,22 @@
 def testfn(x):
-    return 34*x
+    return 34 * x
+
+
+def tf2():
+    return 10
+
+
+def main():
+    print(testfn(10) * tf2())
+
+
+# print(__name__)  # this will print the origin of request of this module / script
+# i.e., when run as a script[python program] __name__ = __main__
+# when run as a module from another program/scipt, __name__ = <name of module> [here it is simplemath]
+if __name__ == '__main__':
+    # this code is specific to this program - not when it is used as a external module
+    main()
+
+if __name__ == 'simplemath':
+    # this is when used as a external module
+    print("Hope you like our module!")
